0d8205515e503d7be726c08351ca2a5a1a7125ff
Evgeny <Gerasimov777@gmail.com>
2017-03-25 20:54:01 +0300

    HomeWork2 с изменениями:
    1. Перенёс Main в начало
    2. Добавил Common из общей библиотеки
    3.Исправил  ввод некорректного значения, дальше програма не идет
    4.Переименовал метод SortMas в  SortAndPrintMas
    5.В методе SortAndPrintMas создал отдельный массив для сортировки.

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Common;
+namespace HomeWork2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint lenghtMas;
+
+            if (!Utils.TryEnterNumberFromConsole(out lenghtMas))
+            {
+                Console.Read();
+                return;
+            }
+
+            int[] masLenghtMas;
+            masLenghtMas = CreatMas(lenghtMas);
+
+            SortAndPrintMas(masLenghtMas, lenghtMas);
+
+            Console.Read();
+        }
+       
+        static int[] CreatMas(uint lenght)
+        {
+            Console.WriteLine("Массив длиной " + lenght + ":");
+
+            int[] mas = new int[lenght];
+            Random r = new Random();
+
+            for (int i = 0; i < lenght; i++)
+            {
+                mas[i] = r.Next(0, 100);
+                Console.Write(mas[i] + " ");
+            }
+            return mas;
+        }
+
+        static void SortAndPrintMas(int[] mas, uint leghtSortMas)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Массив после пузырьковой сортировки:");
+
+            int[] sortMas = new int[leghtSortMas];
+            sortMas = mas;
+            int temp;
+            for (int i = 0; i < sortMas.Length; i++)
+            {
+                for (int j = i + 1; j < sortMas.Length; j++)
+                {
+                    if (sortMas[i] > sortMas[j])
+                    {
+                        temp = sortMas[i];
+                        sortMas[i] = sortMas[j];
+                        sortMas[j] = temp;
+                    }
+                }
+            }
+            for (int i = 0; i < sortMas.Length; i++)
+            {
+                Console.Write(sortMas[i] + " ");
+            }
+        }
+    }
+}
