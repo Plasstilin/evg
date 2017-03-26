9084310067aa8ba80ed6485aba9fbceb31767737
Evgeny <Gerasimov777@gmail.com>
2017-03-26 11:13:31 +0300

    HomeWork1 Полностью переделал. Не сохранил старого исходника.

---
+++ Lesson1/Program.cs
@@ -0,0 +1,54 @@
+﻿using Common;
+using System;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void PrintFibNumberLessThanNumber(uint number)
+        {
+            Console.Write("Числа Фибоначчи меньшие числа " + number.ToString() + ": ");
+            uint firstFibNumber = 0;
+            uint secondFibNumber = 1;
+            if (number != 0)
+            {
+                Console.Write(firstFibNumber + " ");
+            }
+
+            while (secondFibNumber < number)
+            {
+                Console.Write(secondFibNumber + " ");
+                uint nextSecondNumber = firstFibNumber + secondFibNumber;
+                firstFibNumber = secondFibNumber;
+                secondFibNumber = nextSecondNumber;
+            }
+
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            uint number1;
+            uint number2;
+            uint number3;
+
+            if (!Utils.TryEnterNumberFromConsole(out number1) ||
+                !Utils.TryEnterNumberFromConsole(out number2) ||
+                !Utils.TryEnterNumberFromConsole(out number3))
+            {
+                Console.Read();
+                return;
+            }
+
+            uint minNumber = Math.Min(Math.Min(number1, number2), number3);
+            Console.Write("Минимальное число: ");
+            Console.WriteLine(minNumber);
+
+            PrintFibNumberLessThanNumber(number1);
+            PrintFibNumberLessThanNumber(number2);
+            PrintFibNumberLessThanNumber(number3);
+
+            Console.Read();
+        }
+    }
+}
