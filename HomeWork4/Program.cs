1421d4be43f574c4162f40cb12aeacd1faa4aa4e
Evgeny <Gerasimov777@gmail.com>
2017-04-02 15:28:10 +0300

    HomeWork4 исправил: 1. Все переменные double, приватные с маленькой буквы 2. Добавил обязательную проверку на положительное значение 3.Убрал конструктор родительского класса 4 Строгое присвоение имен фигур

--- HomeWork4/Program.cs
+++ HomeWork4/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HomeWork4
 {
@@ -10,17 +6,35 @@ namespace HomeWork4
     {
         static void Main(string[] args)
         {
+            Rectangle rectangle = new Rectangle(9, 6, "название фигуры");
+            Sector sector = new Sector(0, 15, "название фигуры");
+            Circle circle = new Circle(10, "название фигуры");
+            Triangle triangle = new Triangle(8, -9, 9, "название фигуры");
 
-            Rectangle rectangle = new Rectangle(5, 6, "Прямоугольник");
-            Sector sector = new Sector(8, 15, "Сектор");
-            Circle circle = new Circle(10, "Круг");
-            Triangle triangle = new Triangle(8, 5, 9, "Треугольник");
+            if (rectangle.IsError())
+            {
+                Console.WriteLine("{0}:\nПлощадь = {1}. Периметр = {2}\n", rectangle.GetName(), rectangle.AreaOfTheFigure(), rectangle.PerimetrOfTheFigure());
+            }
+            else Console.WriteLine("{0}:\nЗначение должно быть больше 0\n", rectangle.GetName());
 
-            Console.WriteLine("{0}:\nПлощадь = {1}. Периметр = {2}\n", rectangle.GetName(), rectangle.AreaOfTheFigure(), rectangle.PerimetrOfTheFigure());          
-            Console.WriteLine("{0}:\nПлощадь = {1}. Периметр = {2}\n", sector.GetName(), sector.AreaOfTheFigure(), sector.PerimetrOfTheFigure());         
-            Console.WriteLine("{0}:\nПлощадь = {1}. Периметр = {2}\n", circle.GetName(), circle.AreaOfTheFigure(), circle.PerimetrOfTheFigure());
-            Console.WriteLine("{0}:\nПлощадь = {1}. Периметр = {2}\n", triangle.GetName(), triangle.AreaOfTheFigure(), triangle.PerimetrOfTheFigure());
+            if (sector.IsError())
+            {
+                Console.WriteLine("{0}:\nПлощадь = {1}. Периметр = {2}\n", sector.GetName(), sector.AreaOfTheFigure(), sector.PerimetrOfTheFigure());
+            }
+            else Console.WriteLine("{0}:\nЗначение должно быть больше 0\n", sector.GetName());
 
+            if (circle.IsError())
+            {
+                Console.WriteLine("{0}:\nПлощадь = {1}. Периметр = {2}\n", circle.GetName(), circle.AreaOfTheFigure(), circle.PerimetrOfTheFigure());
+            }
+            else Console.WriteLine("{0}:\nЗначение должно быть больше 0\n", circle.GetName());
+
+            if (triangle.IsError())
+            {
+                Console.WriteLine("{0}:\nПлощадь = {1}. Периметр = {2}\n", triangle.GetName(), triangle.AreaOfTheFigure(), triangle.PerimetrOfTheFigure());
+            }
+            else Console.WriteLine("{0}:\nЗначение должно быть больше 0\n", triangle.GetName());
+           
             Console.Read();
         }
     }
