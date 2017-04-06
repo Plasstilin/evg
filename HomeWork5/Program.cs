c282785cdd23947133aff9a6bb42a061c6abe6ad
Evgeny <Gerasimov777@gmail.com>
2017-04-06 23:41:51 +0300

    HomeWork5

---
+++ HomeWork5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HomeWork5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Car car = new Car(4,5);
+
+            Console.WriteLine($"{car.ModelName} имеет {car.HowManyWheels} колёса и {car.HowManyDoors} дверей");
+
+            foreach (Detail i in car.detailMas)
+            {
+                (i as IRotatable)?.Move();
+                (i as IDoor)?.Open();
+                (i as IDoor)?.Open();
+            }
+            Console.Read();
+        }
+    }    
+}
