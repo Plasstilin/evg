ee90d58ec9cbc43dc7e155c79da2b962b3f5902a
Evgeny <Gerasimov777@gmail.com>
2017-04-07 15:50:47 +0300

    HomeWork6 Почему создает две одинаковые матрицы?

---
+++ HomeWork6/Matrix.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork6
+{
+    class Matrix
+    {
+        uint line;
+        uint column;
+        int[,] matrix;
+
+        public Matrix(uint line, uint column)
+        {
+            this.line = line;
+            this.column = column;
+        }
+
+        public void CreatMatrix()
+        {
+            matrix = new int[line, column];
+            Random r = new Random();
+            for (int i = 0; i < line; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    matrix[i, j] = r.Next(-100, 100);
+                }
+            }
+        }
+
+        public void PrintMatrix()
+        {
+            for (int i = 0; i < line; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ HomeWork6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {         
+            Console.WriteLine("Введите число строк матрицы: ");
+            uint howManyLine = Convert.ToUInt32 (Console.ReadLine());  
+            
+            Console.WriteLine("Введите число столбцов матрицы: ");
+            uint howManyСolumn = Convert.ToUInt32(Console.ReadLine());
+
+            Matrix A = new Matrix(howManyLine, howManyСolumn);
+
+            Console.WriteLine("Создана матрица А");
+
+            A.CreatMatrix();
+            A.PrintMatrix();
+          
+            Matrix B = new Matrix(howManyLine, howManyСolumn);
+
+            Console.WriteLine("Создана матрица B");
+
+            B.CreatMatrix();   
+            B.PrintMatrix();
+
+            Console.Read();
+        }
+    }
+}
