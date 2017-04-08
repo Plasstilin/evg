92711d1090a37110444d4082ed8cc618bc1a79b2
Evgeny <Gerasimov777@gmail.com>
2017-04-08 21:57:27 +0300

    HomeWork6 Без определителя и "Теплицева матрица".

--- HomeWork6/Matrix.cs
+++ HomeWork6/Matrix.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HomeWork6
 {
@@ -18,17 +14,37 @@ namespace HomeWork6
             this.column = column;
         }
 
-        public void CreatMatrix()
+        public static class Utils
+        {
+            public static bool TryEnterNumberFromConsole(out int number)
+            {
+                Console.Write("Введите число: ");
+                string numberAsString = Console.ReadLine();
+
+                if (!(int.TryParse(numberAsString, out number)))
+                {
+                    Console.Write("Ввели не число");
+                    return false;
+                }
+                return true;
+            }
+        }
+
+        public int[,] CreatMatrix()
         {
             matrix = new int[line, column];
-            Random r = new Random();
+            Random r = new Random((int)DateTime.Now.Ticks & 0x0000FFFF);
+
             for (int i = 0; i < line; i++)
             {
+
                 for (int j = 0; j < column; j++)
                 {
+
                     matrix[i, j] = r.Next(-100, 100);
                 }
             }
+            return matrix;
         }
 
         public void PrintMatrix()
@@ -38,6 +54,62 @@ namespace HomeWork6
                 for (int j = 0; j < column; j++)
                 {
                     Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void AmountMatrixs(int[,] a, int[,] b)
+        {
+            matrix = new int[line, column];
+            for (int i = 0; i < line; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    matrix[i, j] = a[i, j] + b[i, j];
+                }
+            }
+        }
+
+        public void Multiplication(int[,] a, int[,] b)
+        {
+            matrix = new int[line, column];
+
+            for (uint i = 0; i < line; i++)
+            {
+                for (uint j = 0; j < column; j++)
+                {
+                    for (uint r = 0; r < column; r++)
+                    {
+                        matrix[i, j] += a[i, r] * b[r, j];
+                    }
+                }
+            }
+        }
+
+        public void MultiplicationNumber(int[,] a, int number)
+        {
+            matrix = new int[line, column];
+
+            for (int i = 0; i < line; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    matrix[i, j] = a[i, j] * number;
+                }
+            }
+        }
+
+        public void Transpon(int[,] a)
+        {
+            matrix = new int[column, line];
+
+            for (int i = 0; i < column; i++)
+            {
+                for (int j = 0; j < line; j++)
+                {
+                    matrix[i, j] = a[j, i];
+                    Console.Write(matrix[i, j] + " \t ");
                 }
                 Console.WriteLine();
             }
--- HomeWork6/Program.cs
+++ HomeWork6/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Common;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,27 +10,51 @@ namespace HomeWork6
     class Program
     {
         static void Main(string[] args)
-        {         
+        {
+            int number;
+
             Console.WriteLine("Введите число строк матрицы: ");
-            uint howManyLine = Convert.ToUInt32 (Console.ReadLine());  
-            
+            uint howManyLine = Convert.ToUInt32(Console.ReadLine());
+
             Console.WriteLine("Введите число столбцов матрицы: ");
             uint howManyСolumn = Convert.ToUInt32(Console.ReadLine());
 
-            Matrix A = new Matrix(howManyLine, howManyСolumn);
-
+            Matrix A = new Matrix (howManyLine, howManyСolumn);
+           
             Console.WriteLine("Создана матрица А");
-
-            A.CreatMatrix();
+           int[,] a = A.CreatMatrix();
             A.PrintMatrix();
-          
-            Matrix B = new Matrix(howManyLine, howManyСolumn);
 
             Console.WriteLine("Создана матрица B");
+            int [,] b = A.CreatMatrix();
+            A.PrintMatrix();
 
-            B.CreatMatrix();   
-            B.PrintMatrix();
+            Console.WriteLine("Сумма А и B");           
+            A.AmountMatrixs(a, b);
+            A.PrintMatrix();
 
+            Console.WriteLine("Произведение А и B");
+            if (howManyLine == howManyСolumn)
+            {
+                A.Multiplication(a, b);
+                A.PrintMatrix();
+            } else
+            {
+                Console.WriteLine("Учи матчасть!! Нельзя такие матрицы умножать друг на друга!");
+            }
+
+            Console.WriteLine("Произведение матрицы А и числа");          
+            if (!Matrix.Utils.TryEnterNumberFromConsole(out number))
+            {
+                Console.Read();
+                return;
+            }
+            A.MultiplicationNumber(a, number);
+            A.PrintMatrix();
+
+            Console.WriteLine("Транспонирование матрицы А ");
+            A.Transpon(a);
+           
             Console.Read();
         }
     }
